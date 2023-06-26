3abf787379699587ac1f70473ad3a72abde9be5b
Morganero <morganero1@gmail.com>
2023-06-26 06:53:26 -0500

    starting over

--- Recomp_Resource/client/src/index.js
+++ Recomp_Resource/client/src/index.js
@@ -12,10 +12,8 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+root.render(  
     <App />
-  </React.StrictMode>
 );
 
 
